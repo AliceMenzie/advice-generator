9f883688b7657b2b0744a17156a2f3487c6894a8
AliceMenzie <alice.menzie@gmail.com>
2022-03-02 13:28:58 +1100

    update styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'
-import Advice from './Advice';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Advice from "./Advice";
 
-import './App.scss';
+import "./App.scss";
 
-
-const queryClient = new QueryClient()
-
-
+const queryClient = new QueryClient();
 
 function App() {
-
-  
-
   return (
     <QueryClientProvider client={queryClient}>
-     
-<div className='container'>
-<Advice />
-</div>
+      <div className="container">
+        <Advice />
+      </div>
 
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
